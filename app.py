54e73174623bfaf496d6e855613271766d377cd9
rifqi-qi <ainurrifqi.iqi@gmail.com>
2024-09-25 13:41:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,47 +6,38 @@ from sklearn.linear_model import LogisticRegression
 import re
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
-from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-
-# Mengunduh resource yang dibutuhkan NLTK
-nltk.download('punkt')
-nltk.download('stopwords')
-
-# Membuat stemmer bahasa Indonesia menggunakan Sastrawi
-factory = StemmerFactory()
-stemmer = factory.create_stemmer()
 
 # Fungsi untuk preprocessing teks
 def preprocess_text(text):
     # 1. Case Folding: Mengubah semua huruf menjadi huruf kecil
     text = text.lower()
-
+    
     # 2. Menghilangkan angka dan karakter khusus
     text = re.sub(r'\d+', '', text)  # Menghapus angka
     text = re.sub(r'[^\w\s]', '', text)  # Menghapus tanda baca dan karakter khusus
-
+    
     # 3. Tokenisasi: Memecah teks menjadi kata-kata
     words = word_tokenize(text)
-
+    
     # 4. Menghapus stopwords: kata-kata umum yang tidak membawa banyak informasi
     stop_words = set(stopwords.words('indonesian'))
     words = [word for word in words if word not in stop_words]
-
+    
     # 5. Stemming: Mengubah kata ke bentuk dasar menggunakan Sastrawi
+    stemmer = PorterStemmer()
     words = [stemmer.stem(word) for word in words]
-
+    
     # Menggabungkan kata-kata kembali menjadi satu kalimat
     processed_text = ' '.join(words)
-
+    
     return processed_text
 
+
 # Memuat model dan TF-IDF vectorizer yang telah dilatih
-try:
-    model = joblib.load('logistic_regression_model.pkl')
-    tfidf = joblib.load('tfidf_vectorizer.pkl')
-except FileNotFoundError:
-    st.error("Model atau TF-IDF vectorizer tidak ditemukan. Pastikan file 'logistic_regression_model.pkl' dan 'tfidf_vectorizer.pkl' ada di direktori.")
+model = joblib.load('logistic_regression_model.pkl')
+tfidf = joblib.load('tfidf_vectorizer.pkl')
 
 # Judul aplikasi
 st.title("Aplikasi Klasifikasi Berita")
@@ -66,9 +57,11 @@ if st.button("Klasifikasikan"):
 
         # Melakukan prediksi
         prediction = model.predict(text_tfidf)
-        predicted_category = "Kesehatan" if prediction[0] == "Kesehatan" else "Olahraga"
+        predicted_category = "Kesehatan" if prediction [0] ==  "Kesehatan" else "Olahraga"
 
         # Menampilkan hasil prediksi
         st.write(f"Hasil Klasifikasi: **{predicted_category}**")
     else:
         st.write("Silakan masukkan teks berita terlebih dahulu.")
+
+
